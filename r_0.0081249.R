76d07f986264501e4319a78b124ebf49befaa4dc
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-20 16:02:42 +1100

    update

---
+++ r_0.0081249.R
@@ -0,0 +1,22 @@
+setwd('/Users/ivan/Work_directory/TTC')
+library(data.table)
+rm(list=ls(all=TRUE));gc(reset=TRUE);par(mfrow=c(1,1))
+
+## Big Data -- Sample Data!
+train <- as.data.frame(fread("data/train.csv"))
+dim(train)
+sample_size = 170000
+ratio = ncol(train) / sample_size
+train_sample=sample(train, ratio)
+dim(train_sample)
+write.csv(train_sample, 'data/train_sample.csv')
+
+## Try to make something useful
+train_sample <- as.data.frame(fread("data/train_sample.csv"))
+labels <- as.data.frame(fread("data/trainLabels.csv"))
+dim(labels)
+train_with_labels = merge(train_sample, labels, by = 'id')
+dim(train_with_labels)
+test <- as.data.frame(fread("data/test.csv"))
+
+# Categorical values encoding
