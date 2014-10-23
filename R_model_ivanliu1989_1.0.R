555cab31b7c80872e0d70acea6b2fca571339339
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-23 15:18:26 +1100

    update

--- R_model_ivanliu1989_1.0.R
+++ R_model_ivanliu1989_1.0.R
@@ -12,9 +12,9 @@ require(data.table); require(caret); require(reshape)
 ###############
 train <- as.data.frame(fread('data/train.csv'))
 test <- as.data.frame(fread('data/test.csv'))
-labels <- as.data.frame(fread('data/trainLabels.csv'))
-dim(train); dim(test); dim(labels)
-save(train,test,labels, file='data/raw_data.RData')
+trainLabels <- as.data.frame(fread('data/trainLabels.csv'))
+dim(train); dim(test); dim(trainLabels)
+save(train,test,trainLabels, file='data/raw_data.RData')
 # load('data/raw_data.RData')
 
 #################
@@ -23,22 +23,33 @@ save(train,test,labels, file='data/raw_data.RData')
 train_sample_index <- createDataPartition(train$id,p=0.1,list=F)
 train_sample <- train[train_sample_index,]
 dim(train_sample)
-train_sample_labels <- merge(x=train_sample,y=labels,by.x="id", by.y="id")
+# train <- train_sample
+train_sample_labels <- merge(x=train_sample,y=trainLabels,by.x="id", by.y="id")
 names(train_sample_labels)
 table(substr(names(train_sample_labels), 1, 1))
 
 #################################
 ## Categorical values encoding ##
 #################################
-train_sample_labels[train_sample_labels=='YES'] <- 1
-train_sample_labels[train_sample_labels=='NO'] <- 0
-# train_sample_labels[train_sample_labels=='nan'] <- NA
-test[test=='YES'] <- 1
-test[test=='NO'] <- 0
-# test[test=='nan'] <- NA
-head(train_sample_labels); head(test)
-for (name in names(train_sample_labels)){
-    if (substr(names(train_sample_labels), 1, 1)=='x'){
-        
-    }
-}
+# Group variables
+vars <- colnames(train)
+ID <- vars[1]
+labels <- colnames(trainLabels)
+predictors <- vars[c(-1,-92)] #remove ID and one features with too many factors
+targets <- labels[-1] #remove ID
+# Attach the labels to the training data
+train_labels <- merge(x=train, y=trainLabels, by.x='id', by.y='id')
+rm(train_sample)
+rm(train_sample_index)
+# Split the training data into train/valid (95%/5%)
+# Want to keep train large enough to make a good submission if submitwithfulldata = F
+train_labels_index <- createDataPartition(train_labels$id,p=0.95,list=F)
+train <- train_labels[train_labels_index,]
+valid <- train_labels[-train_labels_index,]
+dim(train); dim(valid)
+
+
+
+
+
+
