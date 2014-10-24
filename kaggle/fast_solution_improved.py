bcc372c26599459948b07bdee370a28c73a28cc1
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-10-24 23:10:34 +1100

    update

--- kaggle/fast_solution_improved.py
+++ kaggle/fast_solution_improved.py
@@ -29,12 +29,61 @@ train = 'train.csv'  # path to training file
 label = 'trainLabels.csv'  # path to label file of training data
 test = 'test.csv'  # path to testing file
 
-D = 2 ** 20  # number of weights use for each model, we have 32 of them
+D = 2 ** 24  # number of weights use for each model, we have 32 of them
 alpha = .1   # learning rate for sgd optimization
 
 
 # function, generator definitions ############################################
 
+# MurmurHash function
+def murmur3_x86_32(data, seed = 0):
+    c1 = 0xcc9e2d51
+    c2 = 0x1b873593
+
+    length = len(data)
+    h1 = seed
+    roundedEnd = (length & 0xfffffffc)  # round down to 4 byte block
+    for i in range(0, roundedEnd, 4):
+      # little endian load order
+      k1 = (ord(data[i]) & 0xff) | ((ord(data[i + 1]) & 0xff) << 8) | \
+           ((ord(data[i + 2]) & 0xff) << 16) | (ord(data[i + 3]) << 24)
+      k1 *= c1
+      k1 = (k1 << 15) | ((k1 & 0xffffffff) >> 17) # ROTL32(k1,15)
+      k1 *= c2
+
+      h1 ^= k1
+      h1 = (h1 << 13) | ((h1 & 0xffffffff) >> 19)  # ROTL32(h1,13)
+      h1 = h1 * 5 + 0xe6546b64
+
+    # tail
+    k1 = 0
+
+    val = length & 0x03
+    if val == 3:
+        k1 = (ord(data[roundedEnd + 2]) & 0xff) << 16
+    # fallthrough
+    if val in [2, 3]:
+        k1 |= (ord(data[roundedEnd + 1]) & 0xff) << 8
+    # fallthrough
+    if val in [1, 2, 3]:
+        k1 |= ord(data[roundedEnd]) & 0xff
+        k1 *= c1
+        k1 = (k1 << 15) | ((k1 & 0xffffffff) >> 17)  # ROTL32(k1,15)
+        k1 *= c2
+        h1 ^= k1
+
+    # finalization
+    h1 ^= length
+
+    # fmix(h1)
+    h1 ^= ((h1 & 0xffffffff) >> 16)
+    h1 *= 0x85ebca6b
+    h1 ^= ((h1 & 0xffffffff) >> 13)
+    h1 *= 0xc2b2ae35
+    h1 ^= ((h1 & 0xffffffff) >> 16)
+
+    return h1 & 0xffffffff
+    
 # A. x, y generator
 # INPUT:
 #     path: path to train.csv or test.csv
@@ -66,7 +115,7 @@ def data(path, label_path=None):
                 # note, the build in hash(), although fast is not stable,
                 #       i.e., same value won't always have the same hash
                 #       on different machines
-                x[m] = abs(hash(str(m) + '_' + feat)) % D
+                x[m] = abs(murmur3_x86_32(str(m) + '_' + feat)) % D
                 
         row = line.rstrip().split(',')
         hash_cols = [3,4,34,35,61,64,65,91,94,95]
@@ -74,7 +123,7 @@ def data(path, label_path=None):
         for i in range(10):
             for j in range(i+1,10):
                 o += 1
-                x[o] = abs(hash(row[hash_cols[i]]+"_x_"+row[hash_cols[j]])) % D
+                x[o] = abs(murmur3_x86_32(row[hash_cols[i]]+"_x_"+row[hash_cols[j]])) % D
         # parse y, if provided
         if label_path:
             # use float() to prevent future type casting, [1:] to ignore id
