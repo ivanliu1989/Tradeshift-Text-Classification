33d611e0bfe46a28152324bb7e7b23fc813fddd9
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-09 10:54:50 +1100

    combine

--- R/combined_model.R
+++ R/combined_model.R
@@ -1,15 +1,27 @@
 setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Tradeshift-Text-Classification\\preds')
 require(data.table)
+data_7 <- read.csv('submission_D22_L011_y33_y6.csv')
 data_7 <- data.frame(fread('submission_D22_L011_y33_y6.csv')) # 16897537
 data_7_2 <- data.frame(fread('submission_D22_L011_y33.csv')) # 17442624
 data_3 <- data.frame(fread('quick_start.csv')) # 17987706
 
 dim(data_7)
 dim(data_3)
-head(data_7)
-head(data_3)
+head(data_7, 50)
+head(data_3, 50)
 
 a1 <- 17987706-17442624
 a2 <- 17442624-16897537
 17987706/a1
 17987706/a2
+
+identical(data_7[,1],data_3[,1])
+identical(head(data_7, 50),head(data_3, 50))
+merge(data_7,data_3,by.x = data_7[,1],by.y=data_3[,1])
+
+combined_pred <- 0.75 * data_7[,2] + 0.25 * data_3[,2]
+cbind(head(combined_pred), head(data_7[,2]), head(data_3[,2]))
+new_pred <- cbind.data.frame(id_label = data_7[,1], pred = combined_pred)
+head(new_pred)
+
+write.table(new_pred, file='combined_pred.csv',sep = ',',col.names = T)
