436b4d6aee11e8da849a11669fcbe6de0f32756c
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-11-10 19:26:50 +1100

    y7

--- R/combined_model.R
+++ R/combined_model.R
@@ -1,6 +1,6 @@
 setwd('C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Tradeshift-Text-Classification\\preds')
 require(data.table)
-data_7 <- read.csv('submission_D22_L011_y33_y6_y12.csv')
+data_7 <- read.csv('submission_D22_L011_y33_y6_y12_y7.csv')
 # data_7 <- data.frame(fread('submission_D22_L011_y33_y6.csv')) # 16897537
 # data_7_2 <- data.frame(fread('submission_D22_L011_y33.csv')) # 17442624
 data_3 <- data.frame(fread('quick_start.csv')) # 17987706
@@ -24,4 +24,4 @@ cbind(head(combined_pred), head(data_7[,2]), head(data_3[,2]))
 new_pred <- cbind.data.frame(id_label = data_7[,1], pred = combined_pred)
 head(new_pred, 50)
 
-write.table(new_pred, file='combined_pred_55_2.csv',sep = ',', row.names = F)
+write.table(new_pred, file='combined_pred_55_y33_6_12_7.csv',sep = ',', row.names = F)
